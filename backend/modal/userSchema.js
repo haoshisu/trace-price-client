eff183d69a30b47d08d2b4141271a29c66f6e848
haoshisu <155418630+haoshisu@users.noreply.github.com>
2025-09-11 00:02:21 +0800

    Add files via upload

---
+++ backend/modal/userSchema.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+ {
+  username: { type: String, unique: true, required: true }, // 帳號
+  password: { type: String, required: true }, // 密碼 hash
+  email: { type: String, unique: true, required: true }, // 信箱
+  isDemo: { type: Boolean, default: false },
+  emailNotification: { enabled: Boolean, threshold: Number },
+  products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+ },
+ { collection: "trace-price-user" }
+);
+
+export default mongoose.model("User", userSchema);
